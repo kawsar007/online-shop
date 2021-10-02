259d711c64a0a8830a422ec5e3b54e6006bef4b1
unknown <kawsar007>
2021-10-02 16:15:06 +0600

    Shopping categories design

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import Categories from "./components/Categories";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Home/>
+      <Categories/>
     </div>
   );
 }
